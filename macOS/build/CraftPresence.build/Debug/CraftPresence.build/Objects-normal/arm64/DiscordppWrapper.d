b2951fa2141e378a0186bd74af91fd190ac2eaec
MinePacu <minepacu@gmail.com>
2025-11-08 17:37:13 +0900

    Remove build folder from Git tracking and update .gitignore

--- macOS/build/CraftPresence.build/Debug/CraftPresence.build/Objects-normal/arm64/DiscordppWrapper.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/nohyeonsu/Desktop/projects/CraftPresence/CraftPresence/ThirdParty/DIscordSDK/include/DiscordppWrapper.cpp \
-  /Users/nohyeonsu/Desktop/projects/CraftPresence/CraftPresence/ThirdParty/DIscordSDK/include/DiscordppWrapper.hpp \
-  /Users/nohyeonsu/Desktop/projects/CraftPresence/CraftPresence/ThirdParty/DIscordSDK/include/discordpp.h \
-  /Users/nohyeonsu/Desktop/projects/CraftPresence/CraftPresence/ThirdParty/DIscordSDK/include/cdiscord.h
